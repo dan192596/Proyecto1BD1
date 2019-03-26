78553bb4ac4c3adff83785af75a19089538c66ae
Daniel Perez <dan192596@gmail.com>
2019-03-26 05:46:11 -0600

    Se agrego el controlador y vista de curso

---
+++ Proyecto1BD1/Controllers/CursoController.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Proyecto1BD1.Controllers
+{    
+    public class CursoController : Controller
+    {
+        private readonly IConfiguration configuration;
+        public CursoController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        // GET: Curso
+        public ActionResult Index()
+        {
+            String connectionstring = configuration.GetConnectionString("DefaultConnectionString");
+            SqlConnection connection = new SqlConnection(connectionstring);
+            List<List<String>> al = new List<List<String>>();
+            connection.Open();
+            SqlCommand com = new SqlCommand("SELECT * FROM curso", connection);
+            SqlDataReader sdr = com.ExecuteReader();
+            while (sdr.Read())
+            {
+                List<String> numb = new List<string>();
+                numb.Add(sdr[0].ToString());//Id
+                numb.Add(sdr[1].ToString());//nombre
+                al.Add(numb);
+            }
+            connection.Close();
+            return View(al);
+        }        
+
+        // GET: Curso/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Curso/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                String Nombre = "";                
+                foreach (var item in collection)
+                {
+                    System.Diagnostics.Debug.WriteLine(item.ToString());
+                    switch (item.Key.ToString().ToLower())
+                    {
+                        case "nombre":
+                            Nombre = item.Value;
+                            break;
+                    }
+                }
+                if (Nombre != "")
+                {
+                    String connectionstring = configuration.GetConnectionString("DefaultConnectionString");
+                    SqlConnection connection = new SqlConnection(connectionstring);
+                    connection.Open();
+                    String sql = "INSERT INTO curso(nombre) VALUES(@param1)";
+                    using (SqlCommand cmd = new SqlCommand(sql, connection))
+                    {
+                        cmd.Parameters.Add("@param1", SqlDbType.VarChar, 255).SqlValue = Nombre;
+                        cmd.CommandType = CommandType.Text;
+                        cmd.ExecuteNonQuery();
+                    }
+                    connection.Close();
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Curso/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Curso/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                String Nombre = "";
+                foreach (var item in collection)
+                {
+                    System.Diagnostics.Debug.WriteLine(item.ToString());
+                    switch (item.Key.ToString().ToLower())
+                    {
+                        case "nombre":
+                            Nombre = item.Value;
+                            break;
+                    }
+                }
+                if (Nombre != "")
+                {
+                    String connectionstring = configuration.GetConnectionString("DefaultConnectionString");
+                    SqlConnection connection = new SqlConnection(connectionstring);
+                    connection.Open();
+                    String sql = "UPDATE curso SET nombre = @param1 WHERE curso.curso = @param2";
+                    using (SqlCommand cmd = new SqlCommand(sql, connection))
+                    {
+                        cmd.Parameters.Add("@param1", SqlDbType.VarChar, 255).SqlValue = Nombre;
+                        cmd.Parameters.Add("@param2", SqlDbType.Int).SqlValue = id;
+                        cmd.CommandType = CommandType.Text;
+                        cmd.ExecuteNonQuery();
+                    }
+                    connection.Close();
+                }
+                return RedirectToAction(nameof(Index));
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Curso/Delete/5
+        public ActionResult Delete(int id)
+        {
+            String connectionstring = configuration.GetConnectionString("DefaultConnectionString");
+            SqlConnection connection = new SqlConnection(connectionstring);
+            List<String> numb = new List<string>(); ;
+            connection.Open();
+            String sql = " SELECT * FROM curso WHERE curso.curso = @param1";
+            using (SqlCommand cmd = new SqlCommand(sql, connection))
+            {
+                cmd.Parameters.Add("@param1", SqlDbType.VarChar, 255).SqlValue = id;
+                cmd.CommandType = CommandType.Text;
+                SqlDataReader sdr = cmd.ExecuteReader();
+                while (sdr.Read())
+                {
+                    numb.Add(sdr[0].ToString());//Id
+                    numb.Add(sdr[1].ToString());//Nombre
+                }
+            }
+            connection.Close();
+            return View(numb);
+        }
+
+        // POST: Curso/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                String connectionstring = configuration.GetConnectionString("DefaultConnectionString");
+                SqlConnection connection = new SqlConnection(connectionstring);
+                connection.Open();
+                String sql = "DELETE FROM curso WHERE curso.curso = @param1; ";
+                using (SqlCommand cmd = new SqlCommand(sql, connection))
+                {
+                    cmd.Parameters.Add("@param1", SqlDbType.Int).SqlValue = id;
+                    cmd.CommandType = CommandType.Text;
+                    cmd.ExecuteNonQuery();
+                }
+                connection.Close();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        // GET: Curso/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+    }
+}
